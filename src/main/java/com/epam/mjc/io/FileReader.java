477a33e0c229db22dd7fcc37b2de485204b4f638
KaterynaKucherenko <estorskaya@gmail.com>
2023-01-07 20:32:59 +0300

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,24 @@
 package com.epam.mjc.io;
-
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))){
+            String [] readName = (reader.readLine().split(": "));
+            String name = readName[1];
+            String [] readAge = (reader.readLine().split(": "));
+            int age = Integer.parseInt(readAge[1]);
+            String [] readMail = (reader.readLine().split(": "));
+            String mail = readMail[1];
+            String [] readPhone = (reader.readLine().split(": "));
+            long phone = Long.parseLong(readPhone[1]);
+            return new Profile(name, age, mail, phone);}
+
+        catch (IOException e){
+            e.printStackTrace();
+        }
         return new Profile();
-    }
-}
+    }}
+
